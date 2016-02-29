1bb3c9b0d7c12effd359b43e07d7644d0b9154f6
Ubuntu <ubuntu@ip-172-31-46-71.sa-east-1.compute.internal>
2016-02-29 18:33:48 -0300

    getting container id

--- node/index.js
+++ node/index.js
@@ -19,7 +19,12 @@ var client = redis.createClient('6379', 'redis');
 app.get('/', function(req, res, next) {
   client.incr('counter', function(err, counter) {
     if(err) return next(err);
-    res.send('This page has been viewed ' + counter + ' times!');
+    //res.send('Hello');
+    //res.write(process.env);
+   // var payload = process.env;
+    //payload.counter = counter;
+    //res.json(payload);
+    res.send('This page has been viewed ' + counter + ' times!<br> Container ID: #' + process.env.HOSTNAME);
   });
 });
 
